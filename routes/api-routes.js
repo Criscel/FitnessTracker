bdcb4899e39ddd691022282a0e02e30912cce0ed
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-31 00:42:25 +1100

    started api-route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,14 @@
 const router = require("express").Router();
-const db = require("");  // Import workout model
+const db = require("../models/workout");  // Import workout model
+
+//post exercise
+router.post("/api/workout", ({ body }, res) => {
+    db.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
