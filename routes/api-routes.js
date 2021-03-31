8d289b146ff6cee5d66ed05bba6eb0d5a372282d
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-03-31 14:24:22 +1100

    updated api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,9 @@
+const express = require('express');
 const router = require("express").Router();
+const mongoose = require("mongoose");
+mongoose.connect('localhost:27017/test');
+const Schema = mongoose.Schema;
+
 const db = require("../models/workout");  // Import workout model
 
 //post exercise
@@ -11,4 +16,17 @@ router.post("/api/workout", ({ body }, res) => {
         res.status(400).json(err);
       });
   });
+
+  // Request for getting all workouts
+router.get("/api/workouts", (req, res) => {
+	db.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
   
+
+module.exports = router;
