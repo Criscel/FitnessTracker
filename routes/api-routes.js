76e26acb3adf34ec8e74712fa70adbfccc75e658
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-04-08 23:49:01 +1000

    add/update exercise updated in api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const router = require("express").Router();
-const mongoose = require("mongoose");
-mongoose.connect('localhost:27017/test');
-const Schema = mongoose.Schema;
+// const express = require('express');
+// const router = require("express").Router();
+// const mongoose = require("mongoose");
+// mongoose.connect('localhost:27017/test');
+// const Schema = mongoose.Schema;
 
 const db = require("../models/workout");  // Import workout model
 
+module.exports = (app) => {
+
+// Request for getting all workouts
+app.get("/api/workouts", (req, res) => {
+	db.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
 //post exercise
-router.post("/api/workout", ({ body }, res) => {
+app.post("/api/workout", ({ body }, res) => {
     db.create(body)
       .then(dbTransaction => {
         res.json(dbTransaction);
@@ -17,16 +30,19 @@ router.post("/api/workout", ({ body }, res) => {
       });
   });
 
-  // Request for getting all workouts
-router.get("/api/workouts", (req, res) => {
-	db.find()
-		.then((dbData) => {
-			res.json(dbData);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
+//add exercise
+app.put("api/workout/:id", (req, res) => {
+  db.findByIdAndUpdate(params.id,
+    {
+      $push: { exercise:body }
+    }
+  ) 
+  .then((dbData) => {
+    res.json(dbData);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
   
-
-module.exports = router;
+}
